2cb8db79e6748982c83e57ad16f6e45a16f26fcf
cmarker <cmarker4@gmail.com>
2020-11-21 07:26:57 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
+
 class BankAccount
+    attr_reader :name
+    attr_accessor :status, :balance
+
+    def initialize(name)
+        @name = name
+        @status = "open"
+        @balance = 1000
+    end
+
+    def deposit(deposit_amount)
+        @balance += deposit_amount
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @status == "open" && @balance > 0 ? true : false
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
 
 end
+
